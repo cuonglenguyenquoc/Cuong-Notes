637b2d9e7c2052a857455335a219590a6baf3951
Leo <cuong.lenguyenquoc@gmail.com>
2024-01-03 09:20:26 +0700

    create xCode project

---
+++ Cuong_Notes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cuong_Notes
+//
+//  Created by Macbook on 03/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cuong_Notes/Cuong_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cuong_NotesApp.swift
+//  Cuong_Notes
+//
+//  Created by Macbook on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cuong_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
